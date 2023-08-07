0b19e61b3900f88e22937ec4b8bdde7987a308cd
omathurin <la.revanche.du.poulpi@gmail.com>
2023-08-07 11:09:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,110 +1,37 @@
+import openai
 import streamlit as st
-import websockets
-import asyncio
-import base64
-import json
 
-import pyaudio
+st.title("ChatGPT-like clone")
 
-auth_key = st.secrets["ASSEMBLYAI_API_KEY"]
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-if 'text' not in st.session_state:
-	st.session_state['text'] = 'Listening...'
-	st.session_state['run'] = False
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
- 
-FRAMES_PER_BUFFER = 3200
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 16000
-p = pyaudio.PyAudio()
- 
-# starts recording
-stream = p.open(
-   format=FORMAT,
-   channels=CHANNELS,
-   rate=RATE,
-   input=True,
-   frames_per_buffer=FRAMES_PER_BUFFER
-)
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-def start_listening():
-	st.session_state['run'] = True
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-def stop_listening():
-	st.session_state['run'] = False
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-
-st.title('Get real-time transcription')
-
-start, stop = st.columns(2)
-start.button('Start listening', on_click=start_listening)
-
-stop.button('Stop listening', on_click=stop_listening)
-
-URL = "wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000"
- 
-
-async def send_receive():
-	
-	print(f'Connecting websocket to url ${URL}')
-
-	async with websockets.connect(
-		URL,
-		extra_headers=(("Authorization", auth_key),),
-		ping_interval=5,
-		ping_timeout=20
-	) as _ws:
-
-		r = await asyncio.sleep(0.1)
-		print("Receiving SessionBegins ...")
-
-		session_begins = await _ws.recv()
-		print(session_begins)
-		print("Sending messages ...")
-
-
-		async def send():
-			while st.session_state['run']:
-				try:
-					data = stream.read(FRAMES_PER_BUFFER)
-					data = base64.b64encode(data).decode("utf-8")
-					json_data = json.dumps({"audio_data":str(data)})
-					r = await _ws.send(json_data)
-
-				except websockets.exceptions.ConnectionClosedError as e:
-					print(e)
-					assert e.code == 4008
-					break
-
-				except Exception as e:
-					print(e)
-					assert False, "Not a websocket 4008 error"
-
-				r = await asyncio.sleep(0.01)
-
-
-		async def receive():
-			while st.session_state['run']:
-				try:
-					result_str = await _ws.recv()
-					result = json.loads(result_str)['text']
-
-					if json.loads(result_str)['message_type']=='FinalTranscript':
-						print(result)
-						st.session_state['text'] = result
-						st.markdown(st.session_state['text'])
-
-				except websockets.exceptions.ConnectionClosedError as e:
-					print(e)
-					assert e.code == 4008
-					break
-
-				except Exception as e:
-					print(e)
-					assert False, "Not a websocket 4008 error"
-			
-		send_result, receive_result = await asyncio.gather(send(), receive())
-
-
-asyncio.run(send_receive())
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
