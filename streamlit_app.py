6d901d84312e52bb27da8ace12fd13aad25ef740
omathurin <la.revanche.du.poulpi@gmail.com>
2023-08-07 13:22:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit as st
 
 st.title("ChatGPT-like clone")
 
+models = ['gpt-3.5-turbo', 'text-davinci-003', 'gpt-4']
+model = st.sidebar.selectbox("Select Model", models)
 
 # Replicate Credentials
 with st.sidebar:
@@ -16,12 +18,30 @@ with st.sidebar:
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
-os.environ['OPENAI_API_KEY'] = openai_api
 
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+    if ((model == 'gpt-3.5-turbo') or (model == 'text-davinci-003')):
+        max_ = 4096
+    else:
+        max_ = 8192
+    st.subheader("Advanced Settings")
+    temperature = st.slider("Temperature", min_value=0.1, max_value=1.0, value=0.7, step=0.1)
+    
+    max_tokens = st.slider("Max Tokens", min_value=64, max_value=max_, value=1024, step=64)
+    top_p = st.slider("Top P", min_value=0.1, max_value=1.0, value=1.0, step=0.1)
+    frequency_penalty = st.slider("Frequency Penalty", min_value=0.0, max_value=1.0, value=0.0, step=0.1)
+    presence_penalty = st.slider("Presence Penalty", min_value=0.0, max_value=1.0, value=0.0, step=0.1)
+    n = st.slider("n", min_value=1, max_value=5, value=1, step=1)
+    
+    # Add best_of option for text-davinci-003
+    if model == 'text-davinci-003':
+        best_of = st.slider("Best Of", min_value=1, max_value=5, value=1, step=1)
+
+openai.api_key = openai_api
+
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    #st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state["openai_model"] = model
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
